4da3c6370d20bfa51b10c8303e5cf93e50f0a969
mdkamranalam <kamranalam1234567890@gmail.com>
2025-10-02 22:54:09 +0530

    Only even numbers sort using bubble sort

---
+++ 03_sorting/bubble_sort/problems/sort_even_numbers.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int *arr, int size)
+{
+    bool isSwapped;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        isSwapped = false;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (arr[j] % 2 == 0 && arr[j + 1] % 2 == 0 && arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+                isSwapped = true;
+            }
+        }
+        if (!isSwapped)
+        {
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {40, 22, 6, 8, 54};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "---------- SORT ONLY EVEN NUMBERS USING BUBBLE SORT ----------" << endl;
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    bubbleSort(arr, size);
+    cout << "\nAfter bubble sort: ";
+    printArray(arr, size);
+    cout << "------------------------------------------------------------" << endl;
+
+    return 0;
+}
