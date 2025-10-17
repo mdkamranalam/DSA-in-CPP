bac77174356ae9c47234cb675a1d342e3cc6eb0d
mdkamranalam <kamranalam1234567890@gmail.com>
2025-10-17 20:30:22 +0530

    Done with Selection Sort in Ascending order

---
+++ 03_sorting/selection_sort/problems/sort_in_ascending.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSortAscending(int *arr, int size)
+{
+    for (int i = 0; i < size - 2; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[minIndex])
+                minIndex = j;
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "---------- SELECTION SORT ----------" << endl;
+    cout << "Original array: ";
+    printArray(arr, size);
+    selectionSortAscending(arr, size);
+    cout << "\nAfter selection sort: ";
+    printArray(arr, size);
+    cout << "------------------------------------" << endl;
+
+    return 0;
+}
