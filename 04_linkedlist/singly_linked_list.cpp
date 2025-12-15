96f7918914c7074db62f97fd57bc682fd5d9c20b
mdkamranalam <kamranalam1234567890@gmail.com>
2025-12-15 22:53:41 +0530

    LinkedList: Add insertion(at beginning, at end, at the position) in the single linked list

--- 04_linkedlist/singly_linked_list.cpp
+++ 04_linkedlist/singly_linked_list.cpp
@@ -32,33 +32,127 @@ public:
 // }
 
 // Function to traverse and print the linked list (recursive approach)
-void traverseList(Node *head)
+// void traverseList(Node *head)
+// {
+//     if (head == nullptr)
+//     {
+//         cout << endl;
+//         return;
+//     }
+
+//     cout << head->data;
+
+//     if (head->next != nullptr)
+//     {
+//         cout << " -> ";
+//     }
+
+//     traverseList(head->next);
+// }
+
+// Function to insert a new node at beginning of the list
+Node *insertAtBeginning(Node *head, int value)
 {
+    Node *newNode = new Node(value);
+    newNode->next = head;
+    return newNode;
+}
+
+void printList(Node *head)
+{
+    Node *curr = head;
+
+    while (curr != nullptr)
+    {
+        cout << curr->data;
+        if (curr->next != nullptr)
+        {
+            cout << " -> ";
+        }
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+// Function to insert a new node at the end of the list
+Node *insertAtEnd(Node *head, int value)
+{
+    Node *newNode = new Node(value);
+
     if (head == nullptr)
     {
-        cout << endl;
-        return;
+        return newNode;
     }
 
-    cout << head->data;
+    Node *last = head;
 
-    if (head->next != nullptr)
+    while (last->next != nullptr)
     {
-        cout << " -> ";
+        last = last->next;
     }
 
-    traverseList(head->next);
+    last->next = newNode;
+
+    return head;
+}
+
+// Function to insert a new node at the given position in the list
+Node *insertAtPosition(Node *head, int pos, int value)
+{
+    if (pos < 1)
+    {
+        return head;
+    }
+
+    if (pos == 1)
+    {
+        Node *newNode = new Node(value);
+        newNode->next = head;
+        return newNode;
+    }
+
+    Node *curr = head;
+    for (int i = 1; i < pos - 1 && curr != nullptr; i++)
+    {
+        curr = curr->next;
+    }
+
+    if (curr == nullptr)
+    {
+        return head;
+    }
+
+    Node *newNode = new Node(value);
+
+    newNode->next = curr->next;
+    curr->next = newNode;
+
+    return head;
 }
 
 int main()
 {
     cout << "---------- SINGLE LINKEDLIST ----------" << endl;
     Node *head = new Node(10);
-    head->next = new Node(20);
-    head->next->next = new Node(30);
-    head->next->next->next = new Node(40);
+    // head->next = new Node(20);
+    // head->next->next = new Node(30);
+    // head->next->next->next = new Node(40);
 
-    traverseList(head);
+    // Insert at beginning
+    int value = 1;
+    head = insertAtBeginning(head, value);
+
+    // Insert at the end
+    value = 100;
+    head = insertAtEnd(head, value);
+
+    // Insert at the given position
+    int pos = 3;
+    value = 20;
+    head = insertAtPosition(head, pos, value);
+
+    // traverseList(head);
+    printList(head);
     cout << "---------------------------------------" << endl;
 
     return 0;
